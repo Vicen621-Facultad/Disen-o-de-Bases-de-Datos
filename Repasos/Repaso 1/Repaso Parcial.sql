7a7caf635ce26dd38fbeda06cf6a23d2526038d8
Vicente García Marti <71749981+vicen621@users.noreply.github.com>
2024-10-28 23:47:40 -0300

    Actualizada la funcion del año

--- Repasos/Repaso 1/Repaso Parcial.sql
+++ Repasos/Repaso 1/Repaso Parcial.sql
@@ -15,11 +15,11 @@ WHERE NOT EXISTS (
 SELECT P.DNI, P.nombre, P.apellido, P.domicilio, P.telefono
 FROM Paciente P INNER JOIN Turno T 
 ON P.DNI = T.DNI
-WHERE strftime('%Y', T.fecha) = '2021' -- Pacientes atendidos en 2021
+WHERE YEAR(T.fecha) = '2021' -- Pacientes atendidos en 2021
 AND P.DNI NOT IN (
     SELECT T2.DNI
     FROM Turno T2
-    WHERE strftime('%Y', T2.fecha) = '2019' -- Pacientes atendidos en 2019
+    WHERE YEAR(T2.fecha) = '2019' -- Pacientes atendidos en 2019
 );
 
 -- 4. Listar todos los pacientes que se atendieron con "OSDE" y tambien con "IOMA"
@@ -37,13 +37,13 @@ AND P.DNI IN (
 SELECT E.nombre, E.Apellido, Count(*)
 FROM Especialista E INNER JOIN Turno T
 ON E.Matricula = T.Matricula
-WHERE strftime('%Y', T.fecha) = '2022'
+WHERE YEAR(T.fecha) = '2022'
 GROUP BY E.Matricula;
 
 -- 6. Listar los pacientes que se hayan atendido mas de 5 veces en el año 2020
 SELECT P.DNI, P.nombre, P.apellido
 FROM Paciente P INNER JOIN Turno T
 ON P.DNI = T.DNI
-WHERE strftime('%Y', T.fecha) = '2020'
+WHERE YEAR(T.fecha) = '2020'
 GROUP BY P.DNI
 HAVING COUNT(*) > 5
